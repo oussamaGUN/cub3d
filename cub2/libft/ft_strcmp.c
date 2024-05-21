a06891112b8c689b5c410e48b14707363ee54b72
oussama sabbar <sebbaroussama3@gmail.com>
2024-05-21 19:28:28 +0100

    parsing the info of the map

--- cub2/libft/ft_strcmp.c
+++ cub2/libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ousabbar <ousabbar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 17:02:41 by ousabbar          #+#    #+#             */
-/*   Updated: 2024/05/21 17:06:43 by ousabbar         ###   ########.fr       */
+/*   Updated: 2024/05/21 18:52:51 by ousabbar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
+	if (!s1 || !s2)
+		return (1);
 	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
--- cub2/parsing/parsing_1.c
+++ cub2/parsing/parsing_1.c
@@ -8,25 +8,25 @@ void file_opennig(t_mlx *mlx_data)
 }
 void parse_texture(char **texture, t_mlx *mlx_data)
 {
-    if (!ft_strcmp(texture[0], "NO"))
+    if (!ft_strcmp(texture[0], "NO\n") || !ft_strcmp(texture[0], "NO"))
     {
         if (!texture[1] || !ft_strcmp(texture[1], "\n"))
             printf("no texture for NO\n"), exit(1);
         mlx_data->map_info.NO = ft_strdup(texture[1]);
     }
-    else if (!ft_strcmp(texture[0], "SO"))
+    else if (!ft_strcmp(texture[0], "SO\n") || !ft_strcmp(texture[0], "SO"))
     {
         if (!texture[1] || !ft_strcmp(texture[1], "\n"))
             printf("no texture for SO\n"), exit(1);
         mlx_data->map_info.SO = ft_strdup(texture[1]);
     }
-    else if (!ft_strcmp(texture[0], "WE"))
+    else if (!ft_strcmp(texture[0], "WE\n") || !ft_strcmp(texture[0], "WE"))
     {
         if (!texture[1] || !ft_strcmp(texture[1], "\n"))
             printf("no texture for WE\n"), exit(1);
         mlx_data->map_info.WE = ft_strdup(texture[1]);
     }
-    else if (!ft_strcmp(texture[0], "EA"))
+    else if (!ft_strcmp(texture[0], "EA\n") || !ft_strcmp(texture[0], "EA"))
     {
         if (!texture[1] || !ft_strcmp(texture[1], "\n"))
             printf("no texture for EA\n"), exit(1);
@@ -35,13 +35,13 @@ void parse_texture(char **texture, t_mlx *mlx_data)
 }
 void parse_color(char **texture, t_mlx *mlx_data)
 {
-    if (!ft_strcmp(texture[0], "F"))
+    if (!ft_strcmp(texture[0], "F\n") || !ft_strcmp(texture[0], "F"))
     {
         if (!texture[1] || !ft_strcmp(texture[1], "\n"))
             printf("no texture for F\n"), exit(1);
         mlx_data->map_info.F = ft_strdup(texture[1]);
     }
-    else if (!ft_strcmp(texture[0], "C"))
+    else if (!ft_strcmp(texture[0], "C\n") || !ft_strcmp(texture[0], "C"))
     {
         if (!texture[1] || !ft_strcmp(texture[1], "\n"))
             printf("no texture for C\n"), exit(1);
@@ -53,7 +53,7 @@ void pars_line(t_mlx *mlx_data, char *line)
     char **texture;
     texture = ft_split(line, ' ');
     if (!texture)
-        printf("ft_split fail\n"), exit(1);
+        printf("Error\n"), exit(1);
     parse_texture(texture, mlx_data);
     parse_color(texture, mlx_data);
 }
@@ -72,5 +72,5 @@ void parsing(t_mlx *mlx_data)
 {
     file_opennig(mlx_data);
     file_reading(mlx_data);
-    printf("hello %s\n", mlx_data->map_info.EA);
+    // printf("%s\n", mlx_data->map_info.EA);
 }
