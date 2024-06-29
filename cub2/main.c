e8f089481bacf6fb16e1a208dec435a949cfa047
oussamaGUN <sebbaroussama3@gmail.com>
2024-06-29 23:50:39 +0100

    added a function that check the name validation

--- cub2/main.c
+++ cub2/main.c
@@ -4,7 +4,7 @@
 int main(int ac, char const *av[])
 {
     t_mlx mlx_data;
-    if (ac != 2)
+    if (ac != 2 || ft_check_extension((char *)av[1], ".cub") == 0)
         return (printf("invalid argument\n"), 1);
     mlx_data.map_file.av = ft_strdup(av[1]);
     parsing(&mlx_data);
