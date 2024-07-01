a8cea9d146173fc600dbf3ea875ec24d358fc993
Oussama Sabbar <ousabbar@c4r5p8.1337.ma>
2024-07-01 12:51:35 +0100

    norm error

--- cub2/main.c
+++ cub2/main.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ousabbar <ousabbar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 11:01:39 by ousabbar          #+#    #+#             */
+/*   Updated: 2024/07/01 11:03:25 by ousabbar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "main.h"
 
-int main(int ac, char const *av[])
+int	main(int ac, char *av[])
 {
-    t_mlx mlx_data;
-    if (ac != 2 || ft_check_extension((char *)av[1], ".cub") == 0)
-        return (printf("invalid argument\n"), 1);
-    mlx_data.map_file.av = ft_strdup(av[1]);
-    parsing(&mlx_data);
-    // raycasting(mlx_data);
-    free(mlx_data.map_file.av);
-    free_infos(&mlx_data);
-    free(mlx_data.map_info.mapstr);
-    free_split(mlx_data.map_info.map);
-    return 0;
+	t_mlx	mlx_data;
+
+	if (ac != 2 || ft_check_extension((char *)av[1], ".cub") == 0)
+		return (printf("invalid argument\n"), 1);
+	mlx_data.map_file.av = ft_strdup(av[1]);
+	parsing(&mlx_data);
+	// raycasting(mlx_data);
+	free(mlx_data.map_file.av);
+	free_infos(&mlx_data);
+	free(mlx_data.map_info.mapstr);
+	free_split(mlx_data.map_info.map);
+	return (0);
 }
