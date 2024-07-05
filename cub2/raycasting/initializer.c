0e7a5c733a75449a3723d75e38ae1e7547139052
mofassa1 <fadouachaziz3@gmail.com>
2024-07-05 16:56:59 +0100

    swap width and hieght args in mlx functoins

--- cub2/raycasting/initializer.c
+++ cub2/raycasting/initializer.c
@@ -6,7 +6,7 @@
 /*   By: afadouac <afadouac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:49:07 by afadouac          #+#    #+#             */
-/*   Updated: 2024/07/04 19:33:05 by afadouac         ###   ########.fr       */
+/*   Updated: 2024/07/04 23:30:03 by afadouac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	initialize(t_mlx	*mlx)
 {
 	mlx->mlx = mlx_init();
-	printf("%d %d\n", mlx->map_info.width * SCALE, mlx->map_info.height * SCALE);
-	mlx->win = mlx_new_window(mlx->mlx, mlx->map_info.height * SCALE, mlx->map_info.width * SCALE, "minirt");
-	mlx->img = mlx_new_image(mlx->mlx, mlx->map_info.height * SCALE, mlx->map_info.width * SCALE);
+	printf("%d %d\n", mlx->map_info.width , mlx->map_info.height );
+	mlx->win = mlx_new_window(mlx->mlx, mlx->map_info.width * SCALE, mlx->map_info.height * SCALE, "minirt");
+	mlx->img = mlx_new_image(mlx->mlx, mlx->map_info.width * SCALE, mlx->map_info.height * SCALE);
 	mlx->addr = mlx_get_data_addr(mlx->img, &mlx->bits_per_pixel,
 			&mlx->line_length, &mlx->endian);
 	
