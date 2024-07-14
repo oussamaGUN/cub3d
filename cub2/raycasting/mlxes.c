b49b14515a97f7e5566e61270de92de0b62ca106
mofassa1 <fadouachaziz3@gmail.com>
2024-07-14 14:13:01 +0100

    changing pixelput to work on both windows

--- cub2/raycasting/mlxes.c
+++ cub2/raycasting/mlxes.c
@@ -6,17 +6,26 @@
 /*   By: afadouac <afadouac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 22:06:05 by afadouac          #+#    #+#             */
-/*   Updated: 2024/07/04 19:33:10 by afadouac         ###   ########.fr       */
+/*   Updated: 2024/07/14 13:29:42 by afadouac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../main.h"
 
-void	my_mlx_pixel_put(t_mlx *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_mlx *data, int x, int y, int color, int wind)
 {
 	char	*pixel;
 
-	pixel = data->addr + (y * data->line_length + x * \
-		(data->bits_per_pixel / 8));
-	*(unsigned int *)pixel = color;
+	if (wind == 3)
+	{
+		pixel = data->addr3d + (y * data->line_length3d + x * \
+			(data->bits_per_pixel3d / 8));
+		*(unsigned int *)pixel = color;
+	}
+	else if (wind == 2)
+	{
+		pixel = data->addr + (y * data->line_length + x * \
+			(data->bits_per_pixel / 8));
+		*(unsigned int *)pixel = color;
+	}
 }
