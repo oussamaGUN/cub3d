54cca15b1ca6b948c1c1e529d603b91b12baf202
mofassa1 <fadouachaziz3@gmail.com>
2024-07-15 14:30:57 +0100

    adding function return color

--- cub2/parsing/parsing_tools_two.c
+++ cub2/parsing/parsing_tools_two.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_tools_two.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ousabbar <ousabbar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: afadouac <afadouac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 11:07:56 by ousabbar          #+#    #+#             */
-/*   Updated: 2024/07/01 14:53:44 by ousabbar         ###   ########.fr       */
+/*   Updated: 2024/07/14 23:49:58 by afadouac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,4 +75,48 @@ int	check_rgb(char *texture)
 		return (free_split(split), 0);
 	free_split(split);
 	return (1);
+}
+
+int color_value(t_mlx *data, int flag)
+{
+    char    **split;
+    int        i;
+
+    i = 0;
+    
+
+    if (flag == 1)
+    {
+		split = ft_split(data->map_info.F, ',');
+		if (!split)
+		{
+			printf("split faild");
+			return (free_split(split), 0);
+		}
+		if (ft_atoi(split[0]) > 255 || ft_atoi(split[1]) > 255
+        	|| ft_atoi(split[2]) > 255)
+        	return (free_split(split), 0);
+        data->floor.r = ft_atoi(split[0]);
+        data->floor.g = ft_atoi(split[1]);
+        data->floor.b = ft_atoi(split[2]);
+		data->floor.color = (data->floor.r << 16) | (data->floor.g << 8) | data->floor.b;
+    }
+    else
+    {
+		split = ft_split(data->map_info.C, ',');
+		if (!split)
+		{
+			printf("split faild");
+			return (free_split(split),0);
+		}
+		if (ft_atoi(split[0]) > 255 || ft_atoi(split[1]) > 255
+        	|| ft_atoi(split[2]) > 255)
+        	return (free_split(split), 0);
+        data->ceil.r = ft_atoi(split[0]);
+        data->ceil.g = ft_atoi(split[1]);
+        data->ceil.b = ft_atoi(split[2]);
+		data->ceil.color = (data->ceil.r << 16) | (data->ceil.g << 8) | data->ceil.b;   
+    }
+
+    return (free_split(split), 1);
 }
