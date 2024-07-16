7bf9f8933ba60ebb845604726c9124696b2b91f4
mofassa1 <fadouachaziz3@gmail.com>
2024-07-16 15:15:00 +0100

    remouving mini window

--- cub2/raycasting/initializer.c
+++ cub2/raycasting/initializer.c
@@ -6,7 +6,7 @@
 /*   By: afadouac <afadouac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:49:07 by afadouac          #+#    #+#             */
-/*   Updated: 2024/07/13 17:08:10 by afadouac         ###   ########.fr       */
+/*   Updated: 2024/07/16 14:46:30 by afadouac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	initialize(t_mlx	*mlx)
 {
 	mlx->mlx = mlx_init();
-	mlx->win = mlx_new_window(mlx->mlx, mlx->map_info.width * SCALE, mlx->map_info.height * SCALE, "MINIMAP");
-	mlx->img = mlx_new_image(mlx->mlx, mlx->map_info.width * SCALE, mlx->map_info.height * SCALE);
-	mlx->addr = mlx_get_data_addr(mlx->img, &mlx->bits_per_pixel,
-			&mlx->line_length, &mlx->endian);
+	//mlx->win = mlx_new_window(mlx->mlx, mlx->map_info.width * SCALE, mlx->map_info.height * SCALE, "MINIMAP");
+	// mlx->img = mlx_new_image(mlx->mlx, MINIW, MINIH);
+	// mlx->addr = mlx_get_data_addr(mlx->img, &mlx->bits_per_pixel,
+	// 		&mlx->line_length, &mlx->endian);
 
-
+	mlx->map_info.maptype = 1;
 	mlx->win3d = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "RAYCASTING");
 	mlx->img3d = mlx_new_image(mlx->mlx, WIDTH, HEIGHT);
 	mlx->addr3d = mlx_get_data_addr(mlx->img3d, &mlx->bits_per_pixel3d,
