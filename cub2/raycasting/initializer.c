c6ab95404170346e1235738db35478a32fe5db03
mofassa1 <fadouachaziz3@gmail.com>
2024-07-22 15:03:55 +0100

    init

--- cub2/raycasting/initializer.c
+++ cub2/raycasting/initializer.c
@@ -6,7 +6,7 @@
 /*   By: afadouac <afadouac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:49:07 by afadouac          #+#    #+#             */
-/*   Updated: 2024/07/18 23:04:44 by afadouac         ###   ########.fr       */
+/*   Updated: 2024/07/20 20:01:11 by afadouac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@ void	initialize(t_mlx	*mlx)
 	// mlx->addr = mlx_get_data_addr(mlx->img, &mlx->bits_per_pixel,
 	// 		&mlx->line_length, &mlx->endian);
 
+	mlx->mouves.down = PLAYERVET * 30;
+	mlx->mouves.up = PLAYERVET * 30;
+	mlx->mouves.left = PLAYERVET * 30;
+	mlx->mouves.right = PLAYERVET * 30;
 	mlx->map_info.maptype = 1;
 	mlx->win3d = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "RAYCASTING");
 	mlx->img3d = mlx_new_image(mlx->mlx, WIDTH, HEIGHT);
